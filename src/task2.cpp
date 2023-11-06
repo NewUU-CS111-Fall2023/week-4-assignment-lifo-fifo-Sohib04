8fd164f726ee167cae3c6367190f3ee2fc5ae061
Sohib04 <127374092+Sohib04@users.noreply.github.com>
2023-11-06 01:57:55 -1200

    Update task2.cpp

--- src/task2.cpp
+++ src/task2.cpp
@@ -1,7 +1,41 @@
 #include <iostream>
+#include <stack>
+#include <unordered_map>
+#include <string>
+
 using namespace std;
 
-int main(){
+bool checkExpression(string brackets) {
+    stack<char> st;
+    unordered_map<char, char> closingToOpening = {{')', '('}, {']', '['}, {'}', '{'}};
 
-  return 0;
+    for (char bracket : brackets) {
+        if (bracket == '(' || bracket == '[' || bracket == '{') {
+            st.push(bracket);
+        } else if (bracket == ')' || bracket == ']' || bracket == '}') {
+            if (st.empty() || st.top() != closingToOpening[bracket]) {
+                return false;
+            } else {
+                st.pop();
+            }
+        }
+    }
+
+    return st.empty();
+}
+
+int main() {
+    int n;
+    string brackets;
+    cin >> n;
+    cin.ignore();
+    getline(cin, brackets);
+
+    if (checkExpression(brackets)) {
+        cout << "yes" << endl;
+    } else {
+        cout << "no" << endl;
+    }
+
+    return 0;
 }
